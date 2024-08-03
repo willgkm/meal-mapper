a2a11d0727494f28f72e0ee754b5481954abbcfa
williangk <wkuviatkovski@gmail.com>
2024-08-03 18:17:33 -0300

    removing unnecessary import

--- frontend/meal-mapper/src/App.tsx
+++ frontend/meal-mapper/src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css'
 import Header from './components/header/header'
 import Footer from './components/footer/footer'
 import 'bootstrap/dist/css/bootstrap.min.css';
