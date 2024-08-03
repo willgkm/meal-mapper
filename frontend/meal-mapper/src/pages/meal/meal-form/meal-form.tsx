f31b1bf295f2dd4cf28d830675f028245762428b
williangk <wkuviatkovski@gmail.com>
2024-08-03 18:33:13 -0300

    render foods list in meal form if there is foods available

--- frontend/meal-mapper/src/pages/meal/meal-form/meal-form.tsx
+++ frontend/meal-mapper/src/pages/meal/meal-form/meal-form.tsx
@@ -41,15 +41,15 @@ export default function MealForm() {
       });
   }
 
-  async function submit(event: any){ 
+  async function submit(event: any) {
     event.preventDefault();
-    if(id ){
+    if (id) {
       edit()
     } else {
       create()
     }
   }
-  
+
   async function create() {
     try {
       await axios.post('http://localhost:8910/meal', meal);
@@ -79,81 +79,86 @@ export default function MealForm() {
     const updatedFoods = meal.foods?.filter(food => food.id !== foodId) || [];
     setMeal({ ...meal, foods: updatedFoods });
   }
-  
+
   return (
     <div className='container'>
       <h2> Create Meal </h2>
       <Button href="/meal" className='my-2' variant='secondary'> {"< Back"} </Button>
 
-      <Form onSubmit={submit}>
+      {foods ? (
+        <Form onSubmit={submit}>
 
-        <Form.Group className="mb-3" controlId="form.mealName">
-          <Form.Label>Name</Form.Label>
-          <Form.Control type="text" placeholder="Name"
-            value={meal?.name || ''}
-            onChange={(event) => setMeal({ ...meal, name: event.target.value })} />
-        </Form.Group>
+          <Form.Group className="mb-3" controlId="form.mealName">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" placeholder="Name"
+              value={meal?.name || ''}
+              onChange={(event) => setMeal({ ...meal, name: event.target.value })} />
+          </Form.Group>
 
-        <Row>
-          <Col className='col-6'>
-            <Form.Group className="mb-3" controlId="form.availableFoods">
-              <Form.Label>Foods available</Form.Label>
-              <Table striped bordered hover>
-                <thead className="table-dark">
-                  <tr>
-                    <th>Name</th>
-                    <th className="text-center" style={{ width: "5rem" }}>Add</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {foods.map((item) => (
-                    <tr key={item.id} className="align-middle">
-                      <td>{item.name}</td>
-                      <td className="text-center">
-                        <Button className="mx-1" variant='success' onClick={() => addFood(item)}>
-                          <i className="bi bi-plus"></i>
-                        </Button>
-                      </td>
+          <Row>
+            <Col className='col-6'>
+              <Form.Group className="mb-3" controlId="form.availableFoods">
+                <Form.Label>Foods available</Form.Label>
+                <Table striped bordered hover>
+                  <thead className="table-dark">
+                    <tr>
+                      <th>Name</th>
+                      <th className="text-center" style={{ width: "5rem" }}>Add</th>
                     </tr>
-                  ))}
-                </tbody>
-              </Table>
-            </Form.Group>
-          </Col>
-          <Col className='col-6'>
-            <Form.Group className="mb-3" controlId="form.selectedFoods">
-              <Form.Label>Foods selected</Form.Label>
-              <Table striped bordered hover>
-                <thead className="table-dark">
-                  <tr>
-                    <th className="text-center" style={{ width: "5rem" }}>Remove</th>
-                    <th>Name</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {meal.foods?.map((item) => (
-                    <tr key={item.id} className="align-middle">
-                      <td className="text-center">
-                        <Button className="mx-1" variant='danger' onClick={() => removeFood(item?.id)}>
-                          <i className="bi bi-dash"></i>
-                        </Button>
-                      </td>
-                      <td>{item.name}</td>
+                  </thead>
+                  <tbody>
+                    {foods.map((item) => (
+                      <tr key={item.id} className="align-middle">
+                        <td>{item.name}</td>
+                        <td className="text-center">
+                          <Button className="mx-1" variant='success' onClick={() => addFood(item)}>
+                            <i className="bi bi-plus"></i>
+                          </Button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </Table>
+              </Form.Group>
+            </Col>
+            <Col className='col-6'>
+              <Form.Group className="mb-3" controlId="form.selectedFoods">
+                <Form.Label>Foods selected</Form.Label>
+                <Table striped bordered hover>
+                  <thead className="table-dark">
+                    <tr>
+                      <th className="text-center" style={{ width: "5rem" }}>Remove</th>
+                      <th>Name</th>
                     </tr>
-                  ))}
-                </tbody>
-              </Table>
-            </Form.Group>
-          </Col>
-        </Row>
-        
+                  </thead>
+                  <tbody>
+                    {meal.foods?.map((item) => (
+                      <tr key={item.id} className="align-middle">
+                        <td className="text-center">
+                          <Button className="mx-1" variant='danger' onClick={() => removeFood(item?.id)}>
+                            <i className="bi bi-dash"></i>
+                          </Button>
+                        </td>
+                        <td>{item.name}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </Table>
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row className='d-grid gap-2 d-md-flex justify-content-md-end'>
+            <Button style={{ width: "100px" }} className='me-md-2 mb-2' type="submit" variant="success">
+              Submit
+            </Button>
+          </Row>
+        </Form>
+      ) : (
+        <div>
+          <h4>There is no food available. You can create on clicking on "New Food" on Foods page</h4>
+        </div>
+      )}
 
-        <Row className='d-grid gap-2 d-md-flex justify-content-md-end'>
-          <Button style={{ width: "100px" }} className='me-md-2 mb-2' type="submit" variant="success">
-            Submit
-          </Button>
-        </Row>
-      </Form>
     </div>
   )
 }
